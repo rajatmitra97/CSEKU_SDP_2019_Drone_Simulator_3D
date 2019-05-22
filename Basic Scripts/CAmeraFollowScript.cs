e9e9e54f001902f1ade0700486fd2a254445b261
Rajat Mitra Raja <30545495+rajatmitra97@users.noreply.github.com>
2019-05-23 00:04:01 +0600

    Add files via upload

---
+++ Basic Scripts/CAmeraFollowScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityStandardAssets.Utility;
+
+public class CAmeraFollowScript : MonoBehaviour {
+
+	private Transform ourDrone;
+	// Use this for initialization
+	void Start () {
+		ourDrone = GameObject.FindGameObjectWithTag ("Player").transform;
+		
+	}
+	private Vector3 velocityCameraFollow;
+	public Vector3 behindPosition = new Vector3 (0, 2, -4);
+	public float angle;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = Vector3.SmoothDamp (transform.position, ourDrone.transform.TransformPoint (behindPosition) , ref velocityCameraFollow, 0.1f);
+		transform.rotation = Quaternion.Euler (new Vector3 (angle, ourDrone.GetComponent <DroneMovement> ().currentYRotation, 0));
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
